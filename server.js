ff481d0a56fb9d7f77a99715e47cc4a620d8232a
gemaTaufik <gemataufik19@gmail.com>
2023-06-09 15:10:00 +0700

    tugas selesai

--- server.js
+++ server.js
@@ -2,17 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fs = require ('node:fs')
 
-// const data = [
-//   {String: 'Testing Data', Integer: '12', Float: '1.45', Date: '12 Desember 2017'},
-//   {String: 'Coba Lagi', Integer: '12', Float: '1.45', Date: '12 Desember 2017'},
-//   {String: 'Testing Data', Integer: '12', Float: '1.45', Date: '12 Desember 2017'},
-// ]
-
 
 const app = express()
 
 let rawdata = fs.readFileSync('data.json');
-let server = JSON.parse(rawdata)
+let data = JSON.parse(rawdata)
 
 const port = 3000
 
@@ -26,7 +20,7 @@ app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
-  res.render('index', {server})
+  res.render('index', {data})
 })
 
 app.get('/Add', (req, res) => {
@@ -34,28 +28,28 @@ app.get('/Add', (req, res) => {
   })
 
 app.post('/Add', (req, res) => {
-  console.log(server)
-  server.push({"id": server.length + 1, "String": req.body.String, "Integer": req.body.Integer, "Float": req.body.Float, "Date": req.body.Date || "Kosong", "Boolean": req.body.Boolean})
-  fs.writeFileSync('data.json', JSON.stringify(server,null, 4))
+ 
+  data.push({"id": data.length + 1, "String": req.body.String, "Integer": req.body.Integer, "Float": req.body.Float, "Date": req.body.Date || "Kosong", "Boolean": req.body.Boolean})
+  fs.writeFileSync('data.json', JSON.stringify(data,null, 4))
   res.redirect('/')
 })
 
   app.get('/hapus/:id', (req,res) =>{
     const id = req.params.id
-    server.splice(id, 1)
-    fs.writeFileSync('data.json', JSON.stringify(server,null, 4))
+    data.splice(id, 1)
+    fs.writeFileSync('data.json', JSON.stringify(data,null, 4))
     res.redirect('/')
   })
 
   app.get('/ubah/:id', (req, res) => {
     const id = req.params.id
-    res.render('edit', {item : server[id]})
+    res.render('edit', {item : data[id]})
   })
 
   app.post('/ubah/:id', (req, res) => {
     const id = req.params.id
-    server[id] = ({"id": parseInt(id) + 1, "String": req.body.String, "Integer": req.body.Integer, "Float": req.body.Float, "Date": req.body.Date || "Kosong", "Boolean": req.body.Boolean})
-    fs.writeFileSync('data.json', JSON.stringify(server, null, 4))
+    data[id] = ({"id": parseInt(id) + 1, "String": req.body.String, "Integer": req.body.Integer, "Float": req.body.Float, "Date": req.body.Date || "Kosong", "Boolean": req.body.Boolean})
+    fs.writeFileSync('data.json', JSON.stringify(data, null, 4))
     res.redirect('/')
   })
 
